4b63d12977ea74968b889a7ee7951c1b172d689a
Patrick Carvalho Malta <patrick.malta@dcoimbra.com.br>
2025-12-01 14:55:53 -0400

    feat: erro ts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+//@ts-ignore
 import 'swiper/css';
+//@ts-ignore
+
 import 'swiper/css/navigation';
+//@ts-ignore
+
 import 'swiper/css/pagination';
 
 createRoot(document.getElementById('root')!).render(
